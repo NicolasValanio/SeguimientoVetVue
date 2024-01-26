e4d9c45188375905fd0e2e6d7e98845fd507a945
Nicolas Valenzuela <serviciosvalenzuela1@gmail.com>
2024-01-26 15:16:51 -0500

    Notificaciones Ok

--- Frontend/src/components/Formulario.vue
+++ Frontend/src/components/Formulario.vue
@@ -1,8 +1,46 @@
 <script setup>
-// import { Ref } from 'vue'
+import { reactive } from 'vue'
+import Swal from 'sweetalert2'
 
-// const 
+const Alertas={
+  // Alerta registro
+  AlertaRegistro(mensaje){
+    Swal.fire({
+    // position: 'top-end',
+      icon: 'success',
+      title: `${mensaje}`,
+      showConfirmButton: false,
+      timer: 1500
+    })
+  },
 
+  //   Alerta Error
+  AlertaError(mensaje){
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: `${mensaje}`,
+    })
+  }
+}
+
+const Paciente=reactive({
+  nombre:'',
+  propietario:'',
+  telefono:'',
+  email:'',
+  alta:'',
+  sintomas:''
+})
+
+const validar =()=>{
+  if (Object.values(Paciente).includes('')) {
+    Alertas.AlertaError('Todos los campos son obligatorios')
+    return
+  }else{
+    Alertas.AlertaRegistro('Paciente registrado con éxito')
+  }
+}
 
 </script>
 
@@ -16,16 +54,20 @@
         <span class="text-indigo-600 font-bold">Administralos</span>
     </p>
 
-    <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+    <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+    @submit.prevent="validar">
         <div class="mb-5">
             <label for="mascota" class="block  text-gray-700 font-bold">
                 Nombre Mascota
             </label>
             <input 
                 type="text"
-                placeholder="Nombre de la Mascota"
+                placeholder="Nombre de la Mascota" 
                 id="mascota"
-                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                v-model="Paciente.nombre"
+                >
+                
                 
         </div>
 
@@ -37,8 +79,9 @@
                 type="text"
                 placeholder="Nombre del propietario"
                 id="propietario"
-                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
-                
+                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                v-model="Paciente.propietario">
+                                    
         </div>
 
         <div class="mb-5">
@@ -49,7 +92,8 @@
                 type="number"
                 placeholder="Telefono del propietario"
                 id="telefono"
-                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                v-model="Paciente.telefono">
                 
         </div>
 
@@ -61,7 +105,8 @@
                 type="email"
                 placeholder="Email del propietario"
                 id="email"
-                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">
+                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                v-model="Paciente.email">
                 
         </div>
 
@@ -72,7 +117,8 @@
             <input 
                 type="date"
                 id="alta"
-                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md">     
+                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                v-model="Paciente.alta">     
         </div>
 
         <div class="mb-5">
@@ -82,14 +128,16 @@
             <textarea 
                 placeholder="Describe los síntomas"
                 id="Síntomas"
-                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40">
+                class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
+                v-model="Paciente.sintomas">
             </textarea>
         </div>
 
         <input 
         class="bg-indigo-600 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors w-full p-3 "
         type="submit"
-        value="Registrar Paciente ">
+        value="Registrar Paciente "
+        @click="validar">
     </form>
 </div>
 </template>
