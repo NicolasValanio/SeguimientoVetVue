577288a3f01eb78b6a5a98f419ca662c4f2131ee
Nicolas Valenzuela <serviciosvalenzuela1@gmail.com>
2024-08-02 03:32:19 -0500

    Mejoras de UI + tablerIcons

--- Frontend/src/components/Footer.vue
+++ Frontend/src/components/Footer.vue
@@ -1,15 +1,19 @@
 <script setup>
-
+import { IconPaw } from '@tabler/icons-vue'
 </script>
 
 
 <template >
 
     <footer class="bg-indigo-600 w-100 py-4">
-        <div class="container">
-            <p class="text-white text-center font-bold text-2xl">Seguimiento Vet - Todos los derechos reservados</p>
+        <div class="container ">
+            <p class="text-white text-center font-bold text-2xl" >
+                <span class="inline-block"><IconPaw stroke="2" width="32" height="32" /></span>
+                Seguimiento Vet 
+                <span class="inline-block"><IconPaw stroke="2" width="32" height="32" /></span>
+            </p>
             <p class="text-white text-center font-bold text-xl">
-                <a href="https://github.com/NicolasValanio" target="_blank" rel="noopener noreferrer"> Nicolas Valenzuela&copy; </a>
+                <a href="https://github.com/NicolasValanio" target="_blank" rel="noopener noreferrer"> Dev: Nicol&aacute;s Valenzuela A. </a>
             </p>
         </div>
     </footer>
--- Frontend/src/components/Header.vue
+++ Frontend/src/components/Header.vue
@@ -1,6 +1,13 @@
+<script setup>
+import { IconStethoscope ,IconActivityHeartbeat} from '@tabler/icons-vue'
+</script>
+
 <template >
     <h1 class="font-black text-5xl  text-center ">
+        <span class="inline-block"><IconActivityHeartbeat stroke=2 width="50" /></span>
         Seguimiento Pacientes
-        <span class="text-indigo-600 ">Veterinaria</span>
+        <span class="text-indigo-600 ">Veterinaria </span>
+        <span class="inline-block text-indigo-600 "><IconStethoscope stroke=2 width="50" /></span>
+        
     </h1>
 </template>
